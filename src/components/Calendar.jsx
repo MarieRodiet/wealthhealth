04ab3af9a47c6f90736a3a9af0874b1f569c613f
Marie Moore <68657110+MarieRodiet@users.noreply.github.com>
2022-09-15 10:53:56 +0200

    Change datepicker library, Made Modal Component reusable and independent, Fix Styles

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,61 +1,32 @@
-import CalendarSelect from 'react-select-date';
-import { ReactComponent as CalendarSvg } from './../assets/calendar-solid.svg';
-import { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import { useState } from 'react';
 import { PropTypes } from 'prop-types';
 
-export function addZero(num) {
-  return num > 9 ? num : '0' + num;
-}
-export default function Calendar({ currentDate, setValue, label, name }) {
-  const [showCalendar, setShowCalendar] = useState(false);
+export default function Calendar({ setValue, label, name }) {
   const [selectedDate, setSelectedDate] = useState(new Date());
-
-  useEffect(() => {
-    setShowCalendar(false);
-  }, [setSelectedDate]);
 
   return (
     <div className="calendar">
-      <div className="calendar-container">
-        <label htmlFor={name}>{label ? label : ''}</label>
-        <input
-          readOnly
-          value={
-            addZero(selectedDate?.getDate()) +
-            '-' +
-            addZero(selectedDate?.getMonth()) +
-            '-' +
-            selectedDate?.getFullYear()
-          }
-          className="calendar-container-input"
-          id={name}
-          type="text"
-        />
-        <CalendarSvg
-          className="calendar-container-svg"
-          onClick={() => setShowCalendar(!showCalendar)}
-        />
-      </div>
-      <div style={{ display: showCalendar ? 'block' : 'none' }}>
-        <CalendarSelect
-          defaultValue={{ date: currentDate }}
-          showDateInputField={false}
-          slotInfo={false}
-          onChange={(date) => {
-            setSelectedDate(date);
-          }}
-          onSelect={(e) => {
-            setSelectedDate(e);
-            setValue(name, e.toLocaleDateString());
-          }}
-        />
-      </div>
+      <label className="calendar-label" htmlFor={name}>
+        {label ? label : ''}
+      </label>
+      <DatePicker
+        className="calendar-datepicker"
+        selected={selectedDate}
+        slotInfo={false}
+        onChange={(date) => {
+          setSelectedDate(date);
+        }}
+        onSelect={(e) => {
+          setSelectedDate(e);
+          setValue(name, e.toLocaleDateString());
+        }}
+      />
     </div>
   );
 }
 
 Calendar.propTypes = {
-  currentDate: PropTypes.string,
   setValue: PropTypes.func,
   label: PropTypes.string,
   name: PropTypes.string.isRequired
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,25 @@
-import { ReactComponent as Close } from '../assets/circle-xmark-solid.svg';
-import { useState } from 'react';
-import { Navigate } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 
-export default function Modal({ text }) {
-  const [isClicked, setIsClicked] = useState(false);
+export default function Modal({ label, closeModal }) {
   return (
     <div className="modal">
-      {isClicked && <Navigate to="/employeelist" replace="true" />}
       <div className="modal-content">
-        <span className="modal-content-span">{text}</span>
-        <Close className="modal-content-close" onClick={() => setIsClicked(true)} />
+        <span className="modal-content-span">{label}</span>
+        <svg
+          className="modal-content-close"
+          onClick={closeModal}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512">
+          <path d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM175 208.1L222.1 255.1L175 303C165.7 312.4 165.7 327.6 175 336.1C184.4 346.3 199.6 346.3 208.1 336.1L255.1 289.9L303 336.1C312.4 346.3 327.6 346.3 336.1 336.1C346.3 327.6 346.3 312.4 336.1 303L289.9 255.1L336.1 208.1C346.3 199.6 346.3 184.4 336.1 175C327.6 165.7 312.4 165.7 303 175L255.1 222.1L208.1 175C199.6 165.7 184.4 165.7 175 175C165.7 184.4 165.7 199.6 175 208.1V208.1z" />
+        </svg>
       </div>
     </div>
   );
 }
 
 Modal.propTypes = {
-  text: PropTypes.string
+  /*Modal content */
+  label: PropTypes.string.isRequired,
+  /*onClick Close Modal*/
+  closeModal: PropTypes.func
 };
